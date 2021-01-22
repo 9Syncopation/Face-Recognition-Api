d405a5026bc963676957f58f49f671a3d1f700eb
9Syncopation <9syncopation@gmail.com>
2021-01-22 11:35:41 +0200

    console

--- controllers/register.js
+++ controllers/register.js
@@ -1,46 +1,48 @@
+const handleRegister = (req, res, db, bcrypt) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json("incorrect form submission");
+  }
+  const hash = bcrypt.hashSync(password);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => {
+    console.log("🚀 ~ file: register.js ~ line 31 ~ handleRegister ~ err", err);
 
-const handleRegister = (req, res, db, bcrypt)=> {
-    const {name, email, password} = req.body;
-    if (!name || !email || !password){
-      return   res.status(400).json('incorrect form submission')
-    }
-    const hash = bcrypt.hashSync(password)
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0],
-                name: name,
-                joined: new Date()
-            })
-            .then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch( err => res.status(400).json('unable to register'))
-    
-    // WITHOUT DATA BASE
-    // database.users.push({
-    //         id: '125',
-    //         name: name,
-    //         email: email,
-    //         entries: 0,
-    //         joined: new Date()
-    // })
-    // res.json(database.users[database.users.length-1]);
-}
+    res.status(400).json("unable to register");
+  });
 
-module.exports={
-    handleRegister: handleRegister
+  // WITHOUT DATA BASE
+  // database.users.push({
+  //         id: '125',
+  //         name: name,
+  //         email: email,
+  //         entries: 0,
+  //         joined: new Date()
+  // })
+  // res.json(database.users[database.users.length-1]);
+};
 
-}
+module.exports = {
+  handleRegister: handleRegister,
+};
